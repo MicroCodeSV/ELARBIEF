ee10f5fe7a1e4840521198a65c2118b3958fe08a
Joksan Alvarado <joksan.alvarado@gmail.com>
2016-10-16 18:53:59 -0600

    Varias actualizaciones y mejoras - Cambiado el esquema de control de tiempo fijo a encendido/apagado - Removido el soporte de sensores de temperatura y compas electronico - Cambiado esquema de control de puente H a solo PWM para ahorrar pines y adaptar a circuito en protoshield

--- elarbief-arduino/elarbief-arduino.ino
+++ elarbief-arduino/elarbief-arduino.ino
@@ -3,13 +3,13 @@
 
 AltSoftSerial serieBt;
 
-const int pinMotorL1 = 2;
-const int pinMotorL2 = 4;
-const int pinMotorLP = 3;
+const int pinMotorL1 = 5;
+const int pinMotorL2 = 6;
+//const int pinMotorLP = 3;
 
-const int pinMotorR1 = 7;
-const int pinMotorR2 = 5;
-const int pinMotorRP = 6;
+const int pinMotorR1 = 10;
+const int pinMotorR2 = 11;
+//const int pinMotorRP = 6;
 
 const int pinTemp = A0;
 
@@ -21,8 +21,8 @@ typedef enum { D_ALTO, D_ADELANTE, D_ATRAS, D_IZQUIERDA,
 
 const int velocidadPwm = 224;
 
-const long tiempoAvance = 1000;
-const long tiempoRotacion = 750;
+//const long tiempoAvance = 1000;
+//const long tiempoRotacion = 750;
 
 void setup() {
   Wire.begin();
@@ -56,35 +56,26 @@ void procesarComando(char dato) {
   float temp;
 
   switch (dato) {
-    case '1': //izquierda
-      controlMotor(D_IZQUIERDA);
-      delay(tiempoRotacion);
-      controlMotor(D_ALTO);
-      break;
-    case '2': //derecha
-      controlMotor(D_DERECHA);
-      delay(tiempoRotacion);
-      controlMotor(D_ALTO);
-      break;
-    case '3': //atras
-      controlMotor(D_ATRAS);
-      delay(tiempoAvance);
-      controlMotor(D_ALTO);
-      break;
-    case '4': //adelante
+    case 'A': //adelante
       controlMotor(D_ADELANTE);
-      delay(tiempoAvance);
+      break;
+    case 'B': //atras
+      controlMotor(D_ATRAS);
+      break;
+    case 'C': //izquierda
+      controlMotor(D_IZQUIERDA);
+      break;
+    case 'D': //derecha
+      controlMotor(D_DERECHA);
+      break;
+    case 'a':
+    case 'b':
+    case 'c':
+    case 'd':
+    case 'E':
       controlMotor(D_ALTO);
       break;
-    case '5': //Temperatura
-      tension = analogRead(pinTemp) * 4.65 / 1023.0;
-      temp = (tension - 0.5) * 100.0;
-      //Serial.println(tension);
-      Serial.println(temp);
-      break;
-    case '6':
-      leerCompas();
-      break;
+
   }
 }
 
@@ -93,65 +84,47 @@ void controlMotor(DIRECCION dir) {
     case D_ALTO:
       digitalWrite(pinMotorL1, LOW);
       digitalWrite(pinMotorL2, LOW);
-      analogWrite(pinMotorLP, 0);
+      //analogWrite(pinMotorLP, 0);
 
       digitalWrite(pinMotorR1, LOW);
       digitalWrite(pinMotorR2, LOW);
-      analogWrite(pinMotorRP, 0);
+      //analogWrite(pinMotorRP, 0);
       break;
     case D_ADELANTE:
       digitalWrite(pinMotorL1, HIGH);
       digitalWrite(pinMotorL2, LOW);
-      analogWrite(pinMotorLP, velocidadPwm);
+      //analogWrite(pinMotorLP, velocidadPwm);
 
       digitalWrite(pinMotorR1, HIGH);
       digitalWrite(pinMotorR2, LOW);
-      analogWrite(pinMotorRP, velocidadPwm);
+      //analogWrite(pinMotorRP, velocidadPwm);
       break;
     case D_ATRAS:
       digitalWrite(pinMotorL1, LOW);
       digitalWrite(pinMotorL2, HIGH);
-      analogWrite(pinMotorLP, velocidadPwm);
+      //analogWrite(pinMotorLP, velocidadPwm);
 
       digitalWrite(pinMotorR1, LOW);
       digitalWrite(pinMotorR2, HIGH);
-      analogWrite(pinMotorRP, velocidadPwm);
+      //analogWrite(pinMotorRP, velocidadPwm);
       break;
     case D_IZQUIERDA:
       digitalWrite(pinMotorL1, LOW);
       digitalWrite(pinMotorL2, HIGH);
-      analogWrite(pinMotorLP, velocidadPwm);
+      //analogWrite(pinMotorLP, velocidadPwm);
 
       digitalWrite(pinMotorR1, HIGH);
       digitalWrite(pinMotorR2, LOW);
-      analogWrite(pinMotorRP, velocidadPwm);
+      //analogWrite(pinMotorRP, velocidadPwm);
       break;
     case D_DERECHA:
       digitalWrite(pinMotorL1, HIGH);
       digitalWrite(pinMotorL2, LOW);
-      analogWrite(pinMotorLP, velocidadPwm);
+      //analogWrite(pinMotorLP, velocidadPwm);
 
       digitalWrite(pinMotorR1, LOW);
       digitalWrite(pinMotorR2, HIGH);
-      analogWrite(pinMotorRP, velocidadPwm);
+      //analogWrite(pinMotorRP, velocidadPwm);
       break;
-  }
-}
-
-int leerCompas() {
-  int lectura = 0;
-  Wire.beginTransmission(0x42 >> 1);
-  Wire.write('A');
-  Wire.endTransmission();
-
-  delay(10);
-
-  Wire.requestFrom(0x42 >> 1, 2);
-  if (Wire.available() >= 2) {
-    lectura = Wire.read();
-    lectura <<= 8;
-    lectura |= Wire.read();
-    lectura /= 10;
-    Serial.println(lectura);
   }
 }
